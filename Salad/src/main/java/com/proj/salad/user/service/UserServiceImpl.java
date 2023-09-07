61b99f69b53924c127fb0cf3e1b593eb92a12ba5
JoSangHyun93 <122254531+JoSangHyun93@users.noreply.github.com>
2023-09-07 16:03:40 +0900

    Update UserServiceImpl.java

--- Salad/src/main/java/com/proj/salad/user/service/UserServiceImpl.java
+++ Salad/src/main/java/com/proj/salad/user/service/UserServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.proj.salad.user.dao.UserDAO;
 import com.proj.salad.user.vo.UserVO;
 
-/* 서승희 userService */
 @Service("userService")
 //propagation : 트랜잭션 전파 규칙 설정, REQUIRED : propagation 속성에 관련된 값, 트랜잭션 없으면 새로운 트랜잭션 생성(디폴트 값)
 @Transactional(propagation=Propagation.REQUIRED)  //@Transactional을 이용해 UserService 클래스의 모든 메서드에 트랜잭션을 적용
