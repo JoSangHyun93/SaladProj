46d239b3a02fd5e228c071578ac66508c226907d
JoSangHyun93 <122254531+JoSangHyun93@users.noreply.github.com>
2023-09-07 15:57:47 +0900

    Update NutrientVO.java

--- Salad/src/main/java/com/proj/salad/prod/vo/NutrientVO.java
+++ Salad/src/main/java/com/proj/salad/prod/vo/NutrientVO.java
@@ -3,7 +3,6 @@ package com.proj.salad.prod.vo;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
-/* 영양소 VO / 김동혁 추가 (23.07.18) */
 /* Lombok 미적용시 Getter Setter 생성해주세요!! */
 @Data
 @Component
