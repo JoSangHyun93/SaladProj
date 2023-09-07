ff36c3de86fdd880a5448b9626fecb625bf37b5a
JoSangHyun93 <122254531+JoSangHyun93@users.noreply.github.com>
2023-09-07 15:51:51 +0900

    Update NoticeDAOImpl.java

--- Salad/src/main/java/com/proj/salad/notice/dao/NoticeDAOImpl.java
+++ Salad/src/main/java/com/proj/salad/notice/dao/NoticeDAOImpl.java
@@ -21,67 +21,67 @@ public class NoticeDAOImpl implements NoticeDAO {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 
-	//하유리: 1-1. 리뷰게시판 전체리스트 보기(23.07.16.)
+	// 1-1. 리뷰게시판 전체리스트 보기(23.07.16.)
 	@Override
 	public List<NoticeVO> selectAllNoticeList(Criteria criteria) {
 		return sqlSession.selectList("notice.selectAllNoticeList", criteria);
 	}
 
-	//하유리: 1-2. 게시물 총 개수(23.07.16.)
+	// 1-2. 게시물 총 개수(23.07.16.)
 	@Override
 	public int getTotal() {
 		return sqlSession.selectOne("notice.getTotal");
 	}
 
-	//하유리: 2-1. 리뷰게시판 글쓰기(23.07.16.)
+	// 2-1. 리뷰게시판 글쓰기(23.07.16.)
 	@Override
 	public void insertNotice(NoticeVO noticeVO) {
 		sqlSession.insert("notice.insertNotice", noticeVO);
 	}
 	
-	//하유리: 게시물 번호 가져오기(23.07.20.)
+	// 게시물 번호 가져오기(23.07.20.)
 	@Override
 	public String selectNotice(NoticeVO noticeVO) {
 		return sqlSession.selectOne("notice.selectNotice", noticeVO);
 	}
 	
-	//하유리: 파일 업로드(23.07.20.)
+	// 파일 업로드(23.07.20.)
 	@Override
 	public void insertImage(Map<String, Object> map) throws Exception {
 		sqlSession.insert("notice.insertNoticeImg", map);
 	}
 	
-	//하유리: 3-1. 게시물 상세보기(23.07.16.)
+	// 3-1. 게시물 상세보기(23.07.16.)
 	@Override
 	public NoticeVO detailNotice(int articleNO) {
 		return (NoticeVO) sqlSession.selectOne("notice.detailNotice", articleNO);
 	}
 	
-	//하유리: 3-2. 조회수(23.07.16.)
+	// 3-2. 조회수(23.07.16.)
 	@Override
 	public void updateCnt(int articleNO) {
 		sqlSession.update("notice.updateCnt", articleNO);
 	}
 	
-	//하유리: 이미지 정보 가져오기(23.07.23.)
+	// 이미지 정보 가져오기(23.07.23.)
 	@Override
 	public List<Notice_imageVO> detailImg(int articleNO) {
 		return sqlSession.selectList("notice.detailImg", articleNO);
 	}
 
-	//하유리: 4-2. 게시물 수정하기(23.07.18.)
+	// 4-2. 게시물 수정하기(23.07.18.)
 	@Override
 	public int updateNotice(NoticeVO noticeVO) {
 		return sqlSession.update("notice.updateNotice", noticeVO);
 	}
 
-	//하유리: 5. 게시물 삭제하기(23.07.18.)
+	// 5. 게시물 삭제하기(23.07.18.)
 	@Override
 	public void deleteNotice(int articleNO) {
 		sqlSession.delete("notice.deleteNotice", articleNO);		
 	}
 
-	//하유리: 6-2. 답변 작성(23.07.18.)
+	// 6-2. 답변 작성(23.07.18.)
 	@Override
 	public void replyNotice(NoticeVO noticeVO) {
 		sqlSession.insert("notice.replyNotice", noticeVO);
