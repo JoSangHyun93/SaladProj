78cd58cbb25a1674af2efaeba41076e0a6984c4f
JoSangHyun93 <122254531+JoSangHyun93@users.noreply.github.com>
2023-09-07 15:39:16 +0900

    Update CartVO.java

--- Salad/src/main/java/com/proj/salad/cart/vo/CartVO.java
+++ Salad/src/main/java/com/proj/salad/cart/vo/CartVO.java
@@ -3,7 +3,6 @@ package com.proj.salad.cart.vo;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
-/* 김동혁 VO 추가 (23.07.19) */
 @Data
 @Component("CartVO")
 public class CartVO {
