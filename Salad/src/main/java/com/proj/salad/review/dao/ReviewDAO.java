5d750fb454a7ec2cef36ff4d576152bb26307fc4
JoSangHyun93 <122254531+JoSangHyun93@users.noreply.github.com>
2023-10-19 14:20:54 +0900

    Update ReviewDAO.java

--- Salad/src/main/java/com/proj/salad/review/dao/ReviewDAO.java
+++ Salad/src/main/java/com/proj/salad/review/dao/ReviewDAO.java
@@ -23,7 +23,7 @@ public interface ReviewDAO {
 	// 2-1. 리뷰게시판 글쓰기(23.07.16.)
 	public void insertReview(ReviewVO reviewVO);
 
-	//김동혁: 2-1-1. order 테이블 reviewStatus -> 1로 수정(23.08.02)
+	// 2-1-1. order 테이블 reviewStatus -> 1로 수정(23.08.02)
 	public void updateReviewStatus(ReviewVO reviewVO);
 	
 	// 게시물 번호 가져오기(23.07.20.)
